ece6d77a27c22812471858a7eae2698395fdaa6d
ealsantos <212905821+ealsantos@users.noreply.github.com>
2026-01-19 16:33:06 +0000

    add baseline

---
+++ script.js
@@ -0,0 +1,13 @@
+function Book (title, author, pages, isRead){
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.isRead = isRead
+    this.info = function () {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead}`;
+    }
+}
+
+const theHobbit = new Book ('The Hobbit', 'J.R.R. Tolkien', 295, true)
+
+console.log(theHobbit.info());
