cc7e7f0f3ec108348cf16ca889e831b6d3aaace8
ealsantos <212905821+ealsantos@users.noreply.github.com>
2026-01-20 12:37:40 +0000

    add logic for book display and form submit click prevention

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const myLibrary = []
 
-function Book (title, author, pages, isRead, ID){
+function Book(title, author, pages, isRead, ID) {
     this.title = title
     this.author = author
     this.pages = pages
@@ -8,51 +8,64 @@ function Book (title, author, pages, isRead, ID){
     this.ID = ID
 }
 
-function addBooktoLibrary(title, author, pages, isRead){
+function addBooktoLibrary(title, author, pages, isRead) {
     const bookID = crypto.randomUUID()
     const newBook = new Book(title, author, pages, isRead, bookID)
     myLibrary.push(newBook)
 }
 
- addBooktoLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
- addBooktoLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
- addBooktoLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
- addBooktoLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
- addBooktoLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+addBooktoLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+addBooktoLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+addBooktoLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+addBooktoLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+addBooktoLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
 
- 
-
-function bookDisplay(){
-    const dataTableHeader = document.querySelector('.bookData')
+function bookDisplay() {
+    const dataRows = document.querySelector('.bookData')
     for (const book of myLibrary) {
         const newBookEntry = document.createElement('tr')
-        dataTableHeader.append(newBookEntry)
 
-        const name = book.title
-        const author = book.author
-        const pages = book.pages
-        const isRead = book.isRead
-        const bookID = book.ID
+        const titleInfo = document.createElement("td");
+        titleInfo.textContent = book.title;
+        newBookEntry.appendChild(titleInfo);
 
-        const bookTitle = document.createElement('td')
-        bookTitle.textContent = name
-        newBookEntry.append(bookTitle)
+        const authorInfo = document.createElement("td");
+        authorInfo.textContent = book.author;
+        newBookEntry.appendChild(authorInfo);
 
-        const bookAuthor = document.createElement('td')
-        bookAuthor.textContent = author
-        newBookEntry.append(bookAuthor)
-    } 
+        const pagesInfo = document.createElement("td");
+        pagesInfo.textContent = book.pages;
+        newBookEntry.appendChild(pagesInfo);
+
+        const readInfo = document.createElement("td");
+        readInfo.textContent = book.isRead;
+        newBookEntry.appendChild(readInfo);
+
+        const idInfo = document.createElement("td");
+        idInfo.textContent = book.ID;
+        newBookEntry.appendChild(idInfo);
+
+        dataRows.append(newBookEntry)
+    }
 }
 
 bookDisplay();
 
 
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector(".openBookDialog");
+const closeButton = document.querySelector(".closeBtn");
 
-/*
-** USAR CODIGO ABAIXO NUM PROTOTIPO **
-this.info = function () {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead}`;
-    }
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
 
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
 
-console.log(theHobbit.info());*/
+const formSubmitBtn = document.querySelector('input[type="submit"]')
+
+formSubmitBtn.addEventListener("click", () => {
+    event.preventDefault();
+})
